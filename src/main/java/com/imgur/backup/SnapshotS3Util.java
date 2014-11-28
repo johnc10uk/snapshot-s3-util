c8167c7f0d706d45ddbe3fcd8c6f58ddfd37782a
John Cooper <John.Cooper@smartfocus.com>
2014-11-28 13:01:23 +0000

    string

--- src/main/java/com/imgur/backup/SnapshotS3Util.java
+++ src/main/java/com/imgur/backup/SnapshotS3Util.java
@@ -66,7 +66,7 @@ public class SnapshotS3Util extends Configured implements Tool
     private long mappers        = 1;
     private long bandwidth      = 200;
     private long snapshotTtl    = 0;
-    private long overwrite      = null;
+    private String overwrite      = null;
     
     // S3 options
     private String accessKey    = null;
